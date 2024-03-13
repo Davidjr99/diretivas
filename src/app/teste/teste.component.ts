ecea34b488f608660acfc450ab1c5f81f8163183
David Junior <davidjunior3010@gmail.com>
2024-03-13 15:17:46 -0300

    ngif

---
+++ src/app/teste/teste.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teste',
+  standalone: true,
+  imports: [],
+  templateUrl: './teste.component.html',
+  styleUrl: './teste.component.css'
+})
+export class TesteComponent {
+
+
+}
